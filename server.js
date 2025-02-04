a75ba1c1fcfef0b31843e43d29766546ac30f0d1
Maingart <maingartvlad@outlook.com>
2025-02-05 02:18:07 +1000

    correct static folder name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public')); // Раздача статических файлов
+app.use(express.static('dist')); // Раздача статических файлов
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
